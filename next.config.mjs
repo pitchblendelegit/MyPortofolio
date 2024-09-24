31e68335fd726676d3b231f113448fa6d2ce4eea
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2024-09-24 21:18:40 +0700

    fix untuk deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    telemetry: false, // Menonaktifkan telemetry
+  };
+  
+  module.exports = nextConfig;
+  
